c2ca3a3dae5210a52870aead3388417862139830
Guilherme Toni <guilhermedelly8@gmail.com>
2025-04-03 09:14:16 -0300

    fix: update views.py to add fields to prepare before send data

--- api/views.py
+++ api/views.py
@@ -96,6 +96,104 @@ def format_response_basic_sheet(response_dict):
         },
     }
 
+def generate_dynamic_object(parameters):
+    mapping = {
+        "numEmp": "numEmp",
+        "datAdm": "datAdm",
+        "sitAfa": "sitAfa",
+        "codCar": "codCar",
+        "codEsc": "codEsc",
+        "codFil": "codFil",
+        "indAdm": "indAdm",
+        "numLoc": "numLoc",
+        "codEtb": "codEtb",
+        "numCtp": "numCtp",
+        "codFicFmd": "codFicFmd",
+        "pagSin": "pagSin",
+        "codFicFmd": "codFicFmd",
+        "escVtr": "escVtr",
+        "tipOpe": "tipOpe",
+        "codVinHvi": "codVinHvi",
+        "admeSo": "admeSo",
+        "iniEtbHeb": "iniEtbHeb",
+        "serCtp": "serCtp",
+        "depIrf": "depIrf",
+        "conRho": "conRho",
+        "numCad": "numCad",
+        "apuPonApu": "apuPonApu",
+        "codTmaHes": "codTmaHes",
+        "tipAdmHfi": "tipAdmHfi",
+        "conTovHlo": "conTovHlo",
+        "fimEtbHeb": "fimEtbHeb",
+        "conFinCcu": "conFinCcu",
+        "digCar": "digCar",
+        "depSaf": "depSaf",
+        "cadFol": "cadFol",
+        "fimEvt": "fimEvt",
+        "nomFun": "nomFun",
+        "artCltApu": "artCltApu",
+        "turInt": "turInt",
+        "conTovHfi": "conTovHfi",
+        "conTosHlo": "conTosHlo",
+        "dexCtp": "dexCtp",
+        "modPag": "modPag",
+        "verInt": "verInt",
+        "ponEmb": "ponEmb",
+        "codMotHsa": "codMotHsa",
+        "catAnt": "catAnt",
+        "locTraHlo": "locTraHlo",
+        "estCtp": "estCtp",
+        "tipCon": "tipCon",
+        "codSinHsi": "codSinHsi",
+        "tipSalHsa": "tipSalHsa",
+        "tInAnt": "tInAnt",
+        "natDesHna": "natDesHna",
+        "numCpf": "numCpf",
+        "busHor": "busHor",
+        "tipSex": "tipSex",
+        "socSinHsi": "socSinHsi",
+        "codEstHsa": "codEstHsa",
+        "numPis": "numPis",
+        "tpcPix": "tpcPix",
+        "tpCtBa": "tpCtBa",
+        "possBHHsi": "possBHHsi",
+        "claSalHsa": "claSalHsa",
+        "admAnt": "admAnt",
+        "dcdPis": "dcdPis",
+        "chvPix": "chvPix",
+        "conBan": "conBan",
+        "nivSalHsa": "nivSalHsa",
+        "matAnt": "matAnt",
+        "tipOpc": "tipOpc",
+        "datNas": "datNas",
+        "valSalHsa": "valSalHsa",
+        "onuSce": "onuSce",
+        "datOpc": "datOpc",
+        "perPag": "perPag",
+        "cplSalHsa": "cplSalHsa",
+        "resOnu": "resOnu",
+        "conFgt": "conFgt",
+        "tipApo": "tipApo",
+        "perJur": "perJur",
+        "movSef": "movSef",
+        "recAdi": "recAdi",
+        "rec13S": "rec13S",
+        "emiCar": "emiCar",
+        "codCha": "codCha",
+        "defFis": "defFis",
+        "benRea": "benRea",
+        "cotDef": "cotDef",
+        "racCor": "racCor",
+        "ratEve": "ratEve",
+        "catSef": "catSef",
+        "datInc": "datInc",
+        "horInc": "horInc",
+    }
+
+    return {
+        mapping.get(key, key): value for key, value in parameters.items() if value not in [None, "", []]
+    }
+
 @api_view(['GET'])
 def api_status(request):
     return Response({"status": "API is running"}, status=status.HTTP_200_OK)
@@ -106,7 +204,7 @@ def receive_data(request):
         data = request.data
         p_version = data.get('version')
         p_wsdl = data.get('wsdl')
-        wsdl = f"http://rubiweb.rpl.eng.br:8081/{p_version}/{p_wsdl}?wsdl"
+        wsdl = f"http://rubiweb.rpl.eng.br:8182/{p_version}/{p_wsdl}?wsdl"
         client = Client(wsdl=wsdl)
 
         method = data.get('method')
@@ -139,33 +237,30 @@ def send_basic_sheet(request):
         data = request.data
         p_version = data.get('version')
         p_wsdl = data.get('wsdl')
-        wsdl = f"http://rubiweb.rpl.eng.br:8081/{p_version}/{p_wsdl}?wsdl"
+        wsdl = f"http://rubiweb.rpl.eng.br:8182/{p_version}/{p_wsdl}?wsdl"
         client = Client(wsdl=wsdl)
 
         method = data.get('method')
         body_params = data.get('params', {})
         parameters = body_params.get('parameters', {})
+        treatedParameters = generate_dynamic_object(parameters)
 
         params = {
             "user": body_params.get('user'),
             "password": body_params.get('password'),
             "encryption": body_params.get('encryption'),
-            "parameters": {
-                "numEmp": params.get('codigoEmpresa'),
-                "tipOpe": parameters.get('tipoOperacao'),
-                "numCpf": parameters.get('cpf'),
-                "nomFun": parameters.get('nome'),
-                "datNas": parameters.get('dataDeNascimento'),
-            }
+            "parameters": treatedParameters
         }
 
         response = client.service[method](**params)
 
         response_dict = serialize(response)
+        print(f"response: {response_dict}")
 
-        mounted_response = format_response_basic_sheet(response_dict)
+        # mounted_response = format_response_basic_sheet(response_dict)
 
-        return Response({"data": mounted_response}, status=status.HTTP_200_OK)
+        return Response({"data": response_dict}, status=status.HTTP_200_OK)
 
     except Exception as e:
+        print(f"err: {e}")
         return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
